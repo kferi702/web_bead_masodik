2e611b0d659b6fae69305da89d9cd2faea42579b
Kovács Ferenc <kferi702@gmail.com>
2019-04-08 17:03:37 +0200

    már csak a lapozás kell

--- php/function.php
+++ php/function.php
@@ -1,4 +1,20 @@
 <?php
+////első kettő az oldalakhoz???
+//define('PAGE_PER_NO',8);
+////oldalszámoló
+//function getPagination($count){
+//    $paginationCount=floor($count/PAGE_PER_NO);
+//    $pageModeCount=$count%PAGE_PER_NO;
+//    if(!empty($pageModeCount)){
+//        $paginationCount++;
+//    }
+//    return $paginationCount;
+//}
+
+
+
+
+//bejelntkezés ellenörzése
 function is_logged() {
     if (isset($_SESSION['usern'])) {
         return false;
@@ -6,6 +22,7 @@ function is_logged() {
         return true;
     }
 }
+//menü bejelntkezés szerint
 function menu() {
     if (is_logged()) {
         echo file_get_contents('html/nav_login.html');
--- script/script.js
+++ script/script.js
@@ -1,2 +1,36 @@
+function sortTable(columnName) {
+    var sort = $("#sort").val();
+    $.ajax({
+        url: 'sorting_table.php',
+        type: 'post',
+        data: {
+            columnName: columnName,
+            sort: sort},
+        success: function (valasz) {
+            $("#database tr:not(:first)").remove();
+            
+            $("#database").append(valasz);
+            if (sort == "asc") {
+                $("#sort").val("DESC");
+            } else {
+                $("#sort").val("ASC");
+            }
+        }
+    });
+}
 
+
+$(documnt).ready(function(){
+  function load_data(page)
+  {
+      $.ajax({
+          url:"paging.php",
+          method:"POST",
+          data:{page:page},
+          success:function(valasz){
+              $('#paging_data').html(data);
+          }
+      })
+  }
+})
 
